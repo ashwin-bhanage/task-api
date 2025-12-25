c57f0cec98de3141cbf60830598846afad323429
Ashwin Bhanage <bhanageashwin28@gmail.com>
2025-12-25 23:30:09 +0530

    feat: add get and list users endpoints

--- app/main.py
+++ app/main.py
@@ -25,3 +25,18 @@ def create_user(user: UserCreate, db: Session = Depends(get_db)):
 
     # 4. Return user
     return db_user
+
+
+# Get all users
+@app.get("/users", response_model=list[UserResponse])
+def list_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
+
+# Get user by user_id
+@app.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
